f22dbfb01ccf6be2817ec8d07e5c0aa6c8a858cd
Mingo07-dev <57068725+Mingo07-dev@users.noreply.github.com>
2022-06-18 15:14:12 +0200

    update

--- src/Image.java
+++ src/Image.java
@@ -1,7 +1,38 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
-public class Image extends JPanel {
-    public Image(){
+public class Image extends JLabel {
+    private InputStream imageStream;
+    private BufferedImage image;
+    private BufferedImage resizedImage;
+    private int targetWidth;
+    private int targetHeight;
 
+    public Image(String imageUrl, int targetWidth, int targetHeight ){
+        this.targetHeight = targetHeight;
+        this.targetWidth = targetWidth;
+        this.imageStream = this.getClass().getResourceAsStream(imageUrl);
+        try {
+            assert this.imageStream != null;
+            this.image = ImageIO.read(this.imageStream);
+            this.resizedImage = resizeImage();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.setIcon(new ImageIcon(resizedImage));
+        this.setSize(125, 63);
+    }
+
+    BufferedImage resizeImage() throws IOException {
+        BufferedImage resizedImage = new BufferedImage(this.targetWidth, this.targetHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics2D = resizedImage.createGraphics();
+        graphics2D.drawImage(this.image, 0, 0, this.targetWidth, this.targetHeight, null);
+        graphics2D.dispose();
+        return resizedImage;
     }
 }
--- src/SchermataCorriere.java
+++ src/SchermataCorriere.java
@@ -11,38 +11,26 @@ public class SchermataCorriere {
     public SchermataCorriere(){
         GridBagConstraints c = new GridBagConstraints();
 
-        InputStream imageStream = this.getClass().getResourceAsStream("res/logo.jpg");
-        BufferedImage image;
-        BufferedImage resizedImage;
-        try {
-            assert imageStream != null;
-            image = ImageIO.read(imageStream);
-            resizedImage = resizeImage(image, 125, 63);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        JLabel picLabel = new JLabel(new ImageIcon(resizedImage));
-        picLabel.setSize(125, 63);
-
-
-
+        Image image = new Image("res/logo.jpg",125,63);
 
         c.fill = GridBagConstraints.BOTH;
         c.gridx = 0;
         c.gridy = 0;
-        c.insets = new Insets(0, 0, 720 - picLabel.getWidth(), 1080 - picLabel.getHeight());
+        c.insets = new Insets(0, 0, 657, 855);
         c.anchor = GridBagConstraints.FIRST_LINE_START;
-        Main.schermataCorrierePanel.add(picLabel, c);
 
-    }
 
-    BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
-        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics2D = resizedImage.createGraphics();
-        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
-        graphics2D.dispose();
-        return resizedImage;
+        Main.schermataCorrierePanel.add(image, c);
+
+        JLabel label = new JLabel("ciao");
+        label.setSize(100,100);
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.insets = new Insets(0, 0, 657, 0);
+        c.anchor = GridBagConstraints.FIRST_LINE_END;
+        Main.schermataCorrierePanel.add(label, c);
+
     }
 
 }
