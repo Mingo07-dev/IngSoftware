14ae7a716966766206b386a648e8811e2579e14c
Mingo07-dev <57068725+Mingo07-dev@users.noreply.github.com>
2022-06-20 23:46:41 +0200

    update

--- src/SchermataAutenticazione.java
+++ src/SchermataAutenticazione.java
@@ -22,23 +22,12 @@ public class SchermataAutenticazione {
 
         //CREA L'IMMAGINE LOGO
         Image picLabel = new Image("logo.jpg",400,200);
-
-        JPanel borderView = new JPanel(new BorderLayout(0,100));
-        JPanel border = new JPanel(new BorderLayout(315,0));
-        JPanel flowImage = new JPanel(new FlowLayout());
-        flowImage.add(picLabel);
-        border.add(flowImage, BorderLayout.NORTH);
-
         //FINE IMMAGINE LOGO
 
         // BOTTONE LOGIN
         buttonSchermataLogin = new Button("SchermataAutenticazione",  "Login", 300,50);
         buttonSchermataLogin.changeFontButton("Arial",1, 25);
         buttonSchermataLogin.createListenerButtonChangeView("SchermataLogin");
-        JPanel border1 = new JPanel(new BorderLayout(365,56));
-        JPanel flowLogin = new JPanel(new FlowLayout());
-        flowLogin.add(buttonSchermataLogin);
-        border1.add(flowLogin, BorderLayout.NORTH);
         //FINE BOTTONE LOGIN
 
 
@@ -47,9 +36,6 @@ public class SchermataAutenticazione {
         buttonSchermataRegistrazione = new Button("SchermataAutenticazione",  "Registrazione", 300,50);
         buttonSchermataRegistrazione.changeFontButton("Arial",1, 25);
         buttonSchermataRegistrazione.createListenerButtonChangeView("SchermataRegistrazione");
-        JPanel flowRegistrazione = new JPanel(new FlowLayout());
-        flowRegistrazione.add(buttonSchermataRegistrazione);
-        border1.add(flowRegistrazione, BorderLayout.CENTER);
         //FINE BOTTONE REGISTRAZIONE
 
 
@@ -58,19 +44,6 @@ public class SchermataAutenticazione {
         buttonSchermataRecuperoCredenziali = new Button("SchermataAutenticazione",  "Recupero Credenziali", 300,50);
         buttonSchermataRecuperoCredenziali.changeFontButton("Arial",1, 25);
         buttonSchermataRecuperoCredenziali.createListenerButtonChangeView("SchermataRecuperoCredenziali");
-
-        JPanel flowRecupero = new JPanel(new FlowLayout());
-        flowRecupero.add(buttonSchermataRecuperoCredenziali);
-        border1.add(flowRecupero, BorderLayout.SOUTH);
-
-        borderView.add(border, BorderLayout.NORTH);
-        borderView.add(border1, BorderLayout.SOUTH);
-
-        JPanel sopra = new JPanel((new BorderLayout()));
-        JPanel sotto = new JPanel((new BorderLayout()));
-        Main.schermataAutenticazionePanel.add(sopra, BorderLayout.NORTH);
-        Main.schermataAutenticazionePanel.add(borderView, BorderLayout.CENTER);
-        Main.schermataAutenticazionePanel.add(sotto, BorderLayout.SOUTH);
         //FINE BOTTONE RECUPERO CREDENZIALI
 
 
