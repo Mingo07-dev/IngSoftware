1cb99bdf3f617148b6ae94f41fbaacbe8bdc317a
Mingo07-dev <57068725+Mingo07-dev@users.noreply.github.com>
2022-06-26 00:35:00 +0200

    update

--- src/AggiuntaProduzione.java
+++ src/AggiuntaProduzione.java
@@ -1,20 +1,26 @@
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 
 public class AggiuntaProduzione {
-    private static int[] nomiFarmaci;
-    private static int[] principioAttivo;
-    private static Date[] dataScadenza;
-    private static int[] quantita;
+    private static String[] nomiFarmaci = {"toradol", "proxizal", "tachipirina", "oki"};
+    private static String[] principioAttivo= {"p1","p2","p3","p4"};
+    private static int[] quantita = {2000,1000,5000,5000};
     public AggiuntaProduzione(){
         TimeScheduler task = new TimeScheduler(25, 1);
     }
     public static void aggiuntaProduzioneAzienda() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        for(int i = 0; i < 10; i++){
-            ResultSet takeLastDate;
-            //takeLastDate = Main.dbms_Azienda.getData("SELECT MAX(dbms_azienda.lista_ordini.data_scadenza) FROM dbms_azienda.farmaco WHERE nome_farmaco = '"+ nomiFarmaci[i] +"' AND principio_attivo = '"+ principioAttivo[i] +"';");
-            //Main.dbms_Azienda.setData("INSERT INTO `dbms_azienda`.`farmaco` (`nome_farmaco`, `principio_attivo`, `data_scadenza`, `quantita_disponibile`) VALUES ('" + nomiFarmaci[i] + "', '"+ principioAttivo[i] +"', '" + dataScadenza[i] + "', '" + quantita[i] + "');");
+        Date dateNow = Date.from(Instant.now());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateNow);
+        calendar.add(Calendar.YEAR, 1);
+        dateNow = (Date) calendar.getTime();
+        LocalDate a = dateNow.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        for(int i = 0; i < 4; i++){
+            Main.dbms_Azienda.setData("INSERT INTO `dbms_azienda`.`farmaco` (`nome_farmaco`, `principio_attivo`, `data_scadenza`, `quantita_disponibile`) VALUES ('" + nomiFarmaci[i] + "', '"+ principioAttivo[i] +"', '" + a + "', '" + quantita[i] + "');");
         }
     }
 }
